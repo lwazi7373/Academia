657cbb075bf02d1bad0c8e7969dac3b7d81a7bb3
lwazi7373 <lwazincube7373@gmail.com>
2026-01-17 12:45:10 +0200

    cleaned up lecturer related/domain logic

--- 4. API - Backend Code/controllers/lecturerController.js
+++ 4. API - Backend Code/controllers/lecturerController.js
@@ -1,11 +1,17 @@
 const moduleService = require("../services/moduleService");
 
+/**
+ * Fetches all the modules the coordinator is responsible for
+ * @param {*} req
+ * @param {*} res
+ * @returns modules
+ */
 const getLecturersModules = async (req, res) => {
+  const lecturerId = req.user.userId; // decoded from the auth Middleware
+  const modules = await moduleService.getLecturerModules(lecturerId);
+  return res
+    .status(200)
+    .json({ msg: "Modules retrieved successfully", modules: modules });
+};
 
-        const lecturerId = req.user.userId; // decoded from the auth Middleware
-        const modules = await moduleService.getLecturerModules(lecturerId);
-        return res.status(200).json({ msg: "Modules retrieved successfully", modules: modules });
-
-}
-
-module.exports = { getLecturersModules }
+module.exports = { getLecturersModules };
--- 4. API - Backend Code/routes/lecturerRoutes.js
+++ 4. API - Backend Code/routes/lecturerRoutes.js
@@ -1,15 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const lecturerController = require("../controllers/lecturerController");
-const authenticateToken = require('../middleware/authMiddleware');
-const authorizeRoles = require("../middleware/authRolesMiddleware");
+const authenticateToken = require("../middleware/authMiddleware");
 
 /**
  * GET /api/me/modules
- * Get all the modules under the specific lecturer currently logged in 
+ * Get all the modules under the specific lecturer currently logged in
  * Note : we need the lecturer's id from the middleware, to use as an identifier to query modules
  */
-router.get('/me/lecturer/modules', authenticateToken, lecturerController.getLecturersModules);
+router.get(
+  "/me/lecturer/modules",
+  authenticateToken,
+  lecturerController.getLecturersModules,
+);
 
 module.exports = router;
