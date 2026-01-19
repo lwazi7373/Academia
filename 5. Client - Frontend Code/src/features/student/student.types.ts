e3da55ae6738d358ee7bc35de0f0b6df7f3b59c3
lwazi7373 <lwazincube7373@gmail.com>
2026-01-19 14:49:43 +0200

    added return example

--- 5. Client - Frontend Code/src/features/student/student.types.ts
+++ 5. Client - Frontend Code/src/features/student/student.types.ts
@@ -48,3 +48,59 @@ export interface GetStudentModulePerformanceResponse {
   msg: string;
   modulePerformances: ModulePerformance[];
 }
+
+/**
+ {
+    "msg": "Upcoming assessments retrieved",
+    "assessments": [
+        {
+            "assessmentId": 57,
+            "assessmentName": "Assignment 1",
+            "totalMark": 100,
+            "weighting": "15.00",
+            "dueDate": "2026-02-19T22:00:00.000Z",
+            "daysUntilDue": 32,
+            "module": {
+                "moduleId": 1,
+                "moduleName": "Programming Fundamentals",
+                "moduleCode": "CSC101"
+            },
+            "studentMark": null,
+            "submission": false,
+            "dateSubmitted": null
+        },
+        {
+            "assessmentId": 58,
+            "assessmentName": "Test 1",
+            "totalMark": 100,
+            "weighting": "20.00",
+            "dueDate": "2026-03-14T22:00:00.000Z",
+            "daysUntilDue": 55,
+            "module": {
+                "moduleId": 1,
+                "moduleName": "Programming Fundamentals",
+                "moduleCode": "CSC101"
+            },
+            "studentMark": null,
+            "submission": false,
+            "dateSubmitted": null
+        },
+        {
+            "assessmentId": 59,
+            "assessmentName": "Assignment 2",
+            "totalMark": 100,
+            "weighting": "15.00",
+            "dueDate": "2026-04-09T22:00:00.000Z",
+            "daysUntilDue": 81,
+            "module": {
+                "moduleId": 1,
+                "moduleName": "Programming Fundamentals",
+                "moduleCode": "CSC101"
+            },
+            "studentMark": "68.00",
+            "submission": 1,
+            "dateSubmitted": "2026-02-20T13:30:00.000Z"
+        }
+    ]
+}
+ */
