0bd1c434e49219f3614666aaac2ee90d5e9fa3e3
lwazi7373 <lwazincube7373@gmail.com>
2026-01-19 07:49:12 +0200

    had a / bug on the route

--- 4. API - Backend Code/routes/moduleRoute.js
+++ 4. API - Backend Code/routes/moduleRoute.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const moduleController = require("../controllers/moduleController");
 
 // GET a particular module
-router.get("module/:moduleId", moduleController.getModuleById);
+router.get("/module/:moduleId", moduleController.getModuleById);
 
 module.exports = router;
